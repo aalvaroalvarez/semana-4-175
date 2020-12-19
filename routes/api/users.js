1312216621fecab9410b7d240cc6d94c49d7ff50
aalvaroalvarez <alvanalar@gmail.com>
2020-12-19 13:48:44 -0500

    Add test usuario

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,8 +8,8 @@ const router = routerx();
 router.get('/list', userController.list);
 router.post('/add', auth.verificarAdministrador, userController.add);
 router.put('/update', auth.verificarAdministrador, userController.update);
-router.put('/activate',userController.activate);
-router.put('/deactivate',userController.deactivate);
+router.put('/activate', auth.verificarAdministrador,userController.activate);
+router.put('/deactivate', auth.verificarAdministrador,userController.deactivate);
 
 router.post('/login', userController.login);
 
